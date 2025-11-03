2ad0d5a0ae97ffb2359642ac6cadc1c5ec56e608
berdellon <58087628+berdellon@users.noreply.github.com>
2025-11-03 23:48:48 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js (fichero en la raíz del repo)
+const CACHE_NAME = 'gestor-v1';
+const urlsToCache = [
+  '/',
+  '/static/style.css',
+  '/static/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
